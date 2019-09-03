94546e3982b9034868d7bd04faac9663b285a515
russell13192 <murrayge@mnstate.edu>
2019-09-02 19:37:41 -0700

    Waypoint Updater Working

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+import cte_calculator
+from geometry_msgs.msg import PoseStamped
 import rospy
 from std_msgs.msg import Bool
+from styx_msgs.msg import Lane
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
 from geometry_msgs.msg import TwistStamped
 import math
@@ -18,7 +21,7 @@ of `dbw_enabled`. While in the simulator, its enabled all the time, in the real 
 not be the case. This may cause your PID controller to accumulate error because the car could
 temporarily be driven by a human instead of your controller.
 We have provided two launch files with this node. Vehicle specific values (like vehicle_mass,
-wheel_base) etc should not be altered in these files.
+wheel_base) etc should not be altered in tbhese files.
 We have also provided some reference implementations for PID controller and other utility classes.
 You are free to use them or build your own.
 Once you have the proposed throttle, brake, and steer values, publish it on the various publishers
@@ -84,16 +87,13 @@ class DBWNode(object):
         
 
 
-        # TODO: Subscribe to all the topics you need to
-
         self.loop()
 
     def loop(self):
         rate = rospy.Rate(50) # 50Hz
         while not rospy.is_shutdown():
-            # TODO: Get predicted throttle, brake, and steering using `twist_controller`
+            # Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-
             if not None in (self.current_vel, self.target_vel, self.target_ang, self.final_waypoints):
 
                 # duration calculation
@@ -118,7 +118,6 @@ class DBWNode(object):
 
                 if self.dbw_enabled:
                     self.publish(self.throttle, self.brake, self.steering)
-
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
@@ -138,7 +137,6 @@ class DBWNode(object):
         bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
-
 
     def twist_msg_cb(self, message):
 
@@ -160,7 +158,6 @@ class DBWNode(object):
 
     def current_pose_cb(self, message):
         self.current_pose = message
-
 
 
 
