8e81b5897df131dd0b1c25679418721646f7cbcf
russell13192 <murrayge@mnstate.edu>
2019-09-15 14:30:28 -0700

    tl detector fix

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -139,6 +139,7 @@ class TLDetector(object):
 
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
+        car_wp_idx = -1
         if(self.pose):
             car_wp_idx = self.get_closest_waypoint(self.pose.pose.position.x,self.pose.pose.position.y)
 
